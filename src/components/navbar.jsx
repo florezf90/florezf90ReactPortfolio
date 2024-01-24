53338a7b6a86c3b0473b11350e9c41b54e8960da
florezf90 <florezf90@gmail.com>
2024-01-23 22:51:54 -0600

    re desgined the projects component, added a map function that will create a new card project whenever I add information to the projectsdata.js file instead of hardcoding in the main projects.js file

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,28 @@
 import 'bulma/css/bulma.css';
-import './CSS/navbar.css';
-import  { useState } from 'react';
+import { useState, useEffect } from 'react';
+import './CSS/navbar.css'; // Assuming this file contains the custom styles for the navbar
 
 const Header = () => {
-      const [isActive, setIsActive] = useState(false);
- return (
- <nav className="navbar is-flex is-justify-content-center mt-3" role="navigation" aria-label="main navigation">
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+      navbar.classList.add('is-fixed-top', 'has-background-primary');
+    } else {
+      navbar.classList.remove('is-fixed-top', 'has-background-primary');
+    }
+  };
+
+  return (
+    <nav className="navbar is-flex is-justify-content-center mt-3" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
         <a role="button" className={`navbar-burger ${isActive ? 'is-active' : ''}`} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" onClick={() => setIsActive(!isActive)}>
           <span aria-hidden="true"></span>
@@ -24,6 +41,6 @@ const Header = () => {
       </div>
     </nav>
   );
-}
+};
 
 export default Header;
