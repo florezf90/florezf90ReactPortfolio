cd3cf2d6765c5ce9e0eb41b1646f065f8c0277ea
florezf90 <florezf90@gmail.com>
2024-01-23 18:41:23 -0600

    redesigned the project component

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -4,97 +4,90 @@ import 'bulma/css/bulma.css';
 const ProjectsScreen = () => {
   // Content for the carousel cards
   return (
-    <section className="hero is-fullheight">
-  <div className="hero-body">
-    <div className="container">
-      <h1 className="title is-1 has-text-centered my-6">Projects</h1>
-      <div className="columns is-centered is-variable is-8 mt-6">
-        <div className="column is-5 mt-6 p-4">
-          <div className="card mt-6">
-            <div className="card-content">
-              <p className="title">Project 1</p>
-              <ul>
-                <li>
-                  <span className="icon">
-                  </span>
-                  title
-                </li>
-                <li>
-                  <span className="icon">
-                  </span>
-                  description
-                </li>
-              </ul>
+
+<section className="hero is-fullheight">
+    <div className="hero-body">
+      <div className="container">
+        <h1 className="title is-1 has-text-centered my-6">Projects</h1>
+        <div className="columns is-centered is-variable is-8 mt-6">
+          <div className="column is-5 mt-6 p-4">
+            <div className="card mt-6" style={{ borderRadius: "10px" }}>
+              <div className="card-content">
+                <div className="columns is-mobile">
+                  <div className="column is-one-fifth">
+                    <figure className="image is-96x96">
+                      <img src="placeholder_image_url" alt="Placeholder image" />
+                    </figure>
+                  </div>
+                  <div className="column">
+                    <p className="title is-size-4">Project 1</p>
+                    <p>About project 1...</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="column is-5 mt-6 p-4">
+                      <div className="card mt-6" style={{ borderRadius: "10px" }}>
+              <div className="card-content">
+                <div className="columns is-mobile">
+                  <div className="column is-one-fifth">
+                    <figure className="image is-96x96">
+                      <img src="placeholder_image_url" alt="Placeholder image" />
+                    </figure>
+                  </div>
+                  <div className="column">
+                    <p className="title is-size-4">Project 1</p>
+                    <p>About project 1...</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="column is-5 mt-6 p-4">
-          <div className="card mt-6">
-            <div className="card-content">
-              <p className="title">Project 2</p>
-              <ul>
-                <li>
-                  <span className="icon">
-                  </span>
-                  title
-                </li>
-                <li>
-                  <span className="icon">
-                  </span>
-                  description
-                </li>
-              </ul>
+        <div className="columns is-centered is-variable is-8 mt-6">
+          <div className="column is-5 mt-6 p-4">
+                      <div className="card mt-6" style={{ borderRadius: "10px" }}>
+              <div className="card-content">
+                <div className="columns is-mobile">
+                  <div className="column is-one-fifth">
+                    <figure className="image is-96x96">
+                      <img src="placeholder_image_url" alt="Placeholder image" />
+                    </figure>
+                  </div>
+                  <div className="column">
+                    <p className="title is-size-4">Project 1</p>
+                    <p>About project 1...</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div className="columns is-centered is-variable is-8 mt-6">
-        <div className="column is-5 mt-6 p-4">
-          <div className="card mt-6">
-            <div className="card-content">
-              <p className="title">Project 3</p>
-              <div className="divider"></div>
-              <ul>
-                <li>
-                  <span className="icon">
-                  </span>
-                  title
-                </li>
-                <li>
-                  <span className="icon">
-                  </span>
-                  description
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="column is-5 mt-6 p-4">
-          <div className="card mt-6">
-            <div className="card-content">
-              <p className="title">Project 4</p>
-              <div className="divider"></div>
-              <ul>
-                <li>
-                  <span className="icon">
-                  </span>
-                  title
-                </li>
-                <li>
-                  <span className="icon">
-                  </span>
-                  description
-                </li>
-              </ul>
+          <div className="column is-5 mt-6 p-4">
+                      <div className="card mt-6" style={{ borderRadius: "10px" }}>
+              <div className="card-content">
+                <div className="columns is-mobile">
+                  <div className="column is-one-fifth">
+                    <figure className="image is-96x96">
+                      <img src="placeholder_image_url" alt="Placeholder image" />
+                    </figure>
+                  </div>
+                  <div className="column">
+                    <p className="title is-size-4">Project 1</p>
+                    <p>About project 1...</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</section>
-      
+  </section>
   )
 }
+
+
+
 
 export default ProjectsScreen;
