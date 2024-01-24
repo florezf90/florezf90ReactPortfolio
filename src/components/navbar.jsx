f22f4c9335899146762c4c1d9bd39e3ed1d2e0c4
florezf90 <florezf90@gmail.com>
2024-01-24 02:15:20 -0600

    added animations to the home component and also added the  profile presentation photo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,46 +1,66 @@
-import 'bulma/css/bulma.css';
-import { useState, useEffect } from 'react';
-import './CSS/navbar.css'; // Assuming this file contains the custom styles for the navbar
-
+import "bulma/css/bulma.css";
+import { useState, useEffect } from "react";
+import "./CSS/navbar.css";
 const Header = () => {
   const [isActive, setIsActive] = useState(false);
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   const handleScroll = () => {
-    const navbar = document.querySelector('.navbar');
+    const navbar = document.querySelector(".navbar");
     if (window.scrollY > 0) {
-      navbar.classList.add('is-fixed-top', );
-            navbar.style.background = 'transparent'; // Set the background color to match the page's background
-
+      navbar.classList.add("is-fixed-top");
+      navbar.style.background = "transparent";
     } else {
-      navbar.classList.remove('is-fixed-top', );
-                  navbar.style.background = 'linear-gradient(to left, #000, #001f3f)';
-
+      navbar.classList.remove("is-fixed-top");
+      navbar.style.background = "linear-gradient(to left, #000, #001f3f)";
     }
   };
 
   return (
-<nav className="navbar is-flex is-justify-content-center mt-3" role="navigation" aria-label="main navigation" style={{background: 'linear-gradient(to left, #000, #001f3f)'}}>
+    <nav
+      className="navbar is-flex is-justify-content-center mt-3"
+      role="navigation"
+      aria-label="main navigation"
+      style={{ background: "linear-gradient(to left, #000, #001f3f)" }}
+    >
       <div className="navbar-brand">
-        <a role="button" className={`navbar-burger ${isActive ? 'is-active' : ''}`} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" onClick={() => setIsActive(!isActive)}>
+        <a
+          role="button"
+          className={`navbar-burger ${isActive ? "is-active" : ""}`}
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+          onClick={() => setIsActive(!isActive)}
+        >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
 
-      <div id="navbarBasicExample" className={`navbar-menu ${isActive ? 'is-active' : ''}`}>
+      <div
+        id="navbarBasicExample"
+        className={`navbar-menu ${isActive ? "is-active" : ""}`}
+      >
         <div className="navbar-start">
-          <a className="navbar-item is-hoverable" href="#home">Home</a>
-          <a className="navbar-item is-hoverable" href="#skills">Skills</a>
-          <a className="navbar-item is-hoverable" href="#projects">Projects</a>
-          <a className="navbar-item is-hoverable" href="#contact">Contact Me</a>
+          <a className="navbar-item is-hoverable" href="#home">
+            Home
+          </a>
+          <a className="navbar-item is-hoverable" href="#skills">
+            Skills
+          </a>
+          <a className="navbar-item is-hoverable" href="#projects">
+            Projects
+          </a>
+          <a className="navbar-item is-hoverable" href="#contact">
+            Contact Me
+          </a>
         </div>
       </div>
     </nav>
